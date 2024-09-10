75933406d4f6096e959770c7cfd1eb2f57a60eb8
choir27 <richardchoi54@gmail.com>
2024-09-10 13:15:33 -0400

    add debug for your first test failure

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  await expect(page.getByText("Any browser • Any platform • One API")).toBeVisible();
+  // await expect(page.getByText("Any browser • Any platform • One API")).toBeVisible();
+  await expect(page.getByText("Hatsune Miku")).toBeVisible();
 
 });
